f5295c3b2d070373f96f81d099e9625324a15c89
haytv <haytv@vng.com.vn>
2023-11-19 16:05:11 +0700

    add  snowflake effect

--- zlp_mitool_script.js
+++ zlp_mitool_script.js
@@ -218,3 +218,36 @@ class Analytics {
     });
   }
 }
+
+window.addEventListener("load", function() {
+  function createSnowflake() {
+      const snowflake = document.createElement('div');
+      snowflake.innerHTML = '&#10052;'; // Sử dụng mã HTML cho biểu tượng tuyết
+      snowflake.style.position = 'fixed'; // Thay đổi ở đây
+      snowflake.style.top = '-30px';
+      snowflake.style.color = 'white'; // Đặt màu sắc là trắng
+      snowflake.style.fontSize = Math.random() * 20 + 10 + 'px';
+      snowflake.style.opacity = Math.random();
+      snowflake.style.userSelect = 'none';
+      snowflake.style.pointerEvents = 'none';
+      snowflake.style.left = Math.random() * window.innerWidth + 'px';
+      snowflake.style.animation = `fall-spin ${Math.random() * 3 + 3}s linear infinite`;
+
+      document.body.appendChild(snowflake);
+
+      setTimeout(() => {
+          snowflake.remove();
+      }, (Math.random() * 3 + 3) * 1000);
+  }
+
+  setInterval(createSnowflake, 300);
+
+  const style = document.createElement('style');
+  document.head.appendChild(style);
+  style.sheet.insertRule(`
+      @keyframes fall-spin {
+          0% { transform: translateY(-100vh) rotate(0deg); }
+          100% { transform: translateY(100vh) rotate(360deg); }
+      }
+  `, style.sheet.cssRules.length);
+})
